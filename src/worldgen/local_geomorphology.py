1fb2c4f178e57e4b4ecc92ed61aeb9cdbcd279f6
Jeremy Resch <134104915+jerrecode@users.noreply.github.com>
2026-09-09 22:06:56 +0200

    Version streamwise meander routing authority

--- src/worldgen/local_geomorphology.py
+++ src/worldgen/local_geomorphology.py
@@ -24,7 +24,7 @@ from scipy import ndimage
 
 from .local_hydrology import LocalHydrologySolver, _sample_area_km2
 from .local_orography import edge_anchor_taper, terrain_frame
-LOCAL_GEOMORPHOLOGY_ALGORITHM_REVISION = "metric-source-adaptive-river-v3"
+LOCAL_GEOMORPHOLOGY_ALGORITHM_REVISION = "metric-source-streamwise-meander-v4"
 
 
 from .planet_tiles import (
